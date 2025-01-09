14b86282d9e4c28e19ee53efd8481ea3f1e9e7e2
Sandro Flausino <sjflausino@gmail.com>
2025-01-09 15:29:40 -0300

    remover template temporário e atualizar a estrutura de URLs e visualizações

--- recipes/views.py
+++ recipes/views.py
@@ -6,8 +6,3 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/home.html',status=201, context={'name':'Sandor Clegane',})
 
-def contato(request):
-    return render(request, 'temp/temp.html')
-
-def sobre(request):
-    return HttpResponse("SOBRE! 1")
